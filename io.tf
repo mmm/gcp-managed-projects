e0a1a8017d1d5f6693444832dfeaa6e7fe595c8d
Mark Mims <mark.mims@gmail.com>
2019-06-04 13:51:15 -0600

    working example

---
+++ io.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  default = "us-central1"
+}
+
+variable "billing_account_user" {}
+variable "billing_folder_id" {}
+variable "billing_account_id" {}
+
+variable "gsuite_user" {}
+variable "gsuite_org_id" {}
---
+++ main.tf
@@ -0,0 +1,63 @@
+
+#terraform {
+  #backend "gcs" {}
+#}
+provider "google" {
+  region      = "${var.region}"
+}
+
+#data "google_active_folder" "department1" {
+  #display_name = "Department 1"
+  #parent = "organizations/1234567"
+#}
+#data "google_folder" "my_folder_1" {
+  #folder = "folders/12345"
+  #lookup_organization = true
+#}
+
+#data "google_billing_account" "acct" {
+  #display_name = "My Billing Account"
+  #open         = true
+#}
+
+###################
+# IAM role setup
+###################
+#
+# In your billing org:
+# - `billing_account_user` needs to be a `BillingAccountAdministrator` for the
+#   billing account within the billing org
+#
+# In your gsuite org:
+# - `gsuite_user` needs to be an `OrganizationAdministrator`
+# - `billing_account_user` needs to be:
+#     - a `BillingAccountAdministrator`
+#     - a `ProjectCreator`
+#     - and I added it as an `OrganizationAdministrator` for good measure
+#
+
+resource "google_project" "project_in_billing_folder" {
+  name       = "mmm-yyyy-3"
+  project_id = "mmm-yyyy-3"
+
+  folder_id = "${var.billing_folder_id}"
+  billing_account = "${var.billing_account_id}"
+}
+
+resource "google_project" "gsuite_project" {
+  name       = "mmm-ad-zzzz-3"
+  project_id = "mmm-ad-zzzz-3"
+
+  org_id = "${var.gsuite_org_id}"
+  billing_account = "${var.billing_account_id}"
+}
+
+resource "google_project_iam_binding" "gsuite_project_owner" {
+  project = "mmm-ad-zzzz-3"
+  role    = "roles/owner"
+
+  members = [
+    "user:${var.gsuite_user}",
+    "user:${var.billing_account_user}",
+  ]
+}
